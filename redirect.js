dc8b5e7c64dfa8dbbed1008019df033ec4933ecc
mega0247 <lkwoplei0@gmail.com>
2025-03-15 13:14:03 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,8 @@
-module.exports = (req, res) => {
-  res.writeHead(301, {
-    Location: 'https://earthbluerelocation.com'
-  });
+// api/redirect.js
+export default function handler(req, res) {
+  const { path } = req.query;
+  
+  // Redirect all requests to earthbluerelocation.com
+  res.writeHead(301, { Location: `https://earthbluerelocation.com` });
   res.end();
-};
+}
