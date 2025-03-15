a0fd2f29e1afdd8234c38401d447ead932f47311
mega0247 <lkwoplei0@gmail.com>
2025-03-15 11:52:54 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,22 @@
-// api/redirect.js
-
 module.exports = (req, res) => {
-    const { shortUrl } = req.query; // Extract the short URL from the query
+  const { shortUrl } = req.query;
 
-    // Redirect to the original URL
-    res.redirect(301, 'https://earthbluerelocation.com');
+  // Define the mappings for the short URL (2 characters) to the destination URL
+  const urlMappings = {
+    "50": "https://earthbluerelocation.com",
+    "65": "https://earthbluerelocation.com",
+    "yw": "https://earthbluerelocation.com",
+    "Bx": "https://earthbluerelocation.com",
+    // Add more mappings here...
+  };
+
+  // Find the destination URL based on the short URL
+  const destinationUrl = urlMappings[shortUrl];
+
+  // Redirect to the original URL if found
+  if (destinationUrl) {
+    res.redirect(destinationUrl);
+  } else {
+    res.status(404).send('Not Found');
+  }
 };
