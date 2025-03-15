1083915d6b307340761809d6323b79ebd56e7dd8
allengary799 <allengary799@gmail.com>
2025-03-15 13:29:11 -0400

    Rearranged folder structure and added api folder for redirects

---
+++ api/redirect.js
@@ -0,0 +1,8 @@
+// api/redirect.js
+export default function handler(req, res) {
+  const { path } = req.query;
+
+  // Redirect all requests to earthbluerelocation.com
+  res.writeHead(301, { Location: 'https://earthbluerelocation.com' });
+  res.end();
+}
