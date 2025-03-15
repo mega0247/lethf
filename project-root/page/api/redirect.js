727ab61dccbd1df265153090883a9a43445dedd8
mega0247 <lkwoplei0@gmail.com>
2025-03-15 14:59:27 -0400

    Update redirect.js

--- project-root/page/api/redirect.js
+++ project-root/page/api/redirect.js
@@ -1,3 +1,12 @@
+// pages/api/redirect.js
 export default function handler(req, res) {
-  res.redirect(301, 'https://earthbluerelocation.com');
+  const { path } = req.query;
+  if (path) {
+    // Construct the redirect URL
+    const redirectUrl = `https://earthbluerelocation.com/${path}`;
+    return res.redirect(301, redirectUrl);  // Permanent redirect (301)
+  } else {
+    // Return a 400 if the 'path' query parameter is missing
+    return res.status(400).send('Missing path parameter');
+  }
 }
